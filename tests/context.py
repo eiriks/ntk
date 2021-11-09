6cfd54da6ba9456fb2113edf60e1dfb8c347f0e1
Eirik Stavelin <eirik.stavelin@noaignite.com>
2021-11-09 18:32:54 +0100

    fixing old dependancy

--- tests/context.py
+++ tests/context.py
@@ -3,5 +3,5 @@ import sys
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 
-#import Genie
+# trick from https://docs.python-guide.org/writing/structure/
 from praenomen2genus import Genie
